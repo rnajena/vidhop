0c0f4518e32fe183c4ad17c4b21f87bece32d567
flomock <florian_mock91@web.de>
2019-03-08 10:22:47 +0100

    install fix 7

--- setup.py
+++ setup.py
@@ -22,13 +22,3 @@ setup(
 
 )
 
-
-'''
-> MANIFEST.in tells Distutils what files to include in the source distribution
-but it does not directly affect what files are installed. For that you need to
-include the appropriate files in the setup.py file, generally either as package
-data or as additional files. -- stackoverflow, 3596979
-
-https://docs.python.org/3/distutils/setupscript.html#installing-package-data
-https://docs.python.org/3/distutils/sourcedist.html#manifest
-'''
